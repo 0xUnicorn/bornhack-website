a07e4a25bca8a32905fcb4e7657ecc41033c4745
0xUnicorn <17219001+0xUnicorn@users.noreply.github.com>
2023-05-20 21:12:40 +0200

    Added ProposalBase class to 'EventProposalForm' class and refactored most of the existing labels and help texts

--- src/program/forms.py
+++ src/program/forms.py
@@ -32,30 +32,6 @@ class ProposalBase:
     def add_help_text(self, field, help_text):
         self.fields[field].help_text = help_text
 
-    def name_label_text(self, placeholder):
-        text = f"{placeholder} Name"
-        self.add_label_text("name", text)
-
-    def name_help_text(self, placeholder):
-        text = f"The name of the {placeholder}. Can be a real name or an alias (public)."
-        self.add_help_text("name", text)
-
-    def email_label_text(self, placeholder):
-        text = f"{placeholder} Email"
-        self.add_label_text("email", text)
-
-    def email_help_text(self, placeholder):
-        text = f"The email for this {placeholder}. Will default to the logged-in users email if left empty (not public)."
-        self.add_help_text("email", text)
-
-    def biography_label_text(self, placeholder):
-        text = f"{placeholder} Biography"
-        self.add_label_text("biography", text)
-
-    def biography_help_text(self, placeholder):
-        text = f"The biography of the {placeholder} (public)."
-        self.add_help_text("biography", text)
-
     def submission_notes_help_text(self, placeholder):
         text = f"Private notes regarding this {placeholder} (not public)."
         self.add_help_text("submission_notes", text)
@@ -160,8 +136,32 @@ class SpeakerProposalForm(ProposalBase, forms.ModelForm):
         # fix label and help_text for the submission_notes field
         self.submission_notes_help_text(help_text)
 
+    def name_label_text(self, placeholder):
+        text = f"{placeholder} Name"
+        self.add_label_text("name", text)
 
-class EventProposalForm(forms.ModelForm):
+    def name_help_text(self, placeholder):
+        text = f"The name of the {placeholder}. Can be a real name or an alias (public)."
+        self.add_help_text("name", text)
+
+    def email_label_text(self, placeholder):
+        text = f"{placeholder} Email"
+        self.add_label_text("email", text)
+
+    def email_help_text(self, placeholder):
+        text = f"The email for this {placeholder}. Will default to the logged-in users email if left empty (not public)."
+        self.add_help_text("email", text)
+
+    def biography_label_text(self, placeholder):
+        text = f"{placeholder} Biography"
+        self.add_label_text("biography", text)
+
+    def biography_help_text(self, placeholder):
+        text = f"The biography of the {placeholder} (public)."
+        self.add_help_text("biography", text)
+
+
+class EventProposalForm(ProposalBase, forms.ModelForm):
     """
     The EventProposalForm. Takes an EventType in __init__ and changes fields accordingly.
     """
@@ -204,20 +204,28 @@ class EventProposalForm(forms.ModelForm):
         # TODO: make sure the track is part of the current camp, needs camp as form kwarg to verify
         return track
 
+    def title_label_text(self, placeholder):
+        text = f"Title of {placeholder}"
+        self.add_label_text("title", text)
+
+    def title_help_text(self, placeholder):
+        text = f"The title of this {placeholder}"
+        self.add_help_text("title", text)
+
+    def abstract_label_text(self, placeholder):
+        text = f"Description of {placeholder}"
+        self.add_label_text("abstract", text)
+
+    def abstract_help_text(self, placeholder):
+        text = f"The description of this {placeholder}. Explain what the audience will experience."
+        self.add_help_text("abstract", text)
+
     def __init__(self, camp, event_type=None, matrix=None, *args, **kwargs):
         # initialise form
         super().__init__(*args, **kwargs)
 
         # we need event_type for cleaning later
         self.event_type = event_type
-
-        TALK = "Talk"
-        LIGHTNING_TALK = "Lightning Talk"
-        DEBATE = "Debate"
-        MUSIC_ACT = "Music Act"
-        RECREATIONAL_EVENT = "Recreational Event"
-        WORKSHOP = "Workshop"
-        MEETUP = "Meetup"
 
         # disable the empty_label for the track select box
         self.fields["track"].empty_label = None
@@ -226,13 +234,14 @@ class EventProposalForm(forms.ModelForm):
         # make sure video_recording checkbox defaults to checked
         self.fields["allow_video_recording"].initial = True
 
-        if event_type.name not in [TALK, LIGHTNING_TALK]:
+        if event_type.name not in [self.TALK, self.LIGHTNING_TALK]:
             # Only talk or lightning talk should show the slides_url field
             del self.fields["slides_url"]
 
         # better placeholder text for duration field
         self.fields["duration"].label = f"{event_type.name} Duration"
         self.fields["submission_notes"].label = "Notes to the Content Team"
+
         if event_type.event_duration_minutes:
             self.fields[
                 "duration"
@@ -250,142 +259,62 @@ class EventProposalForm(forms.ModelForm):
                 "placeholder"
             ] = f"{event_type.name} Duration (in minutes)"
 
-        if not event_type.name == LIGHTNING_TALK:
+        if not event_type.name == self.LIGHTNING_TALK:
             # Only lightning talks submissions will have to choose whether to use provided speaker laptop
             del self.fields["use_provided_speaker_laptop"]
 
-        if event_type.name == DEBATE:
-            # fix label and help_text for the title field
-            self.fields["title"].label = "Title of debate"
-            self.fields["title"].help_text = "The title of this debate"
+        if event_type.name == self.DEBATE:
+            label = "debate"
+            help_text = f"{label}"
 
-            # fix label and help_text for the abstract field
-            self.fields["abstract"].label = "Description"
-            self.fields["abstract"].help_text = "The description of this debate"
-
-            # fix label and help_text for the submission_notes field
-            self.fields[
-                "submission_notes"
-            ].help_text = "Private notes regarding this debate. Only visible to yourself and the BornHack organisers."
-
-        elif event_type.name == MUSIC_ACT:
-            # fix label and help_text for the title field
-            self.fields["title"].label = "Title of music act"
-            self.fields["title"].help_text = "The title of this music act/concert/set."
-
-            # fix label and help_text for the abstract field
-            self.fields["abstract"].label = "Description"
-            self.fields["abstract"].help_text = "The description of this music act"
-
-            # fix label and help_text for the submission_notes field
-            self.fields[
-                "submission_notes"
-            ].help_text = "Private notes regarding this music act. Only visible to yourself and the BornHack organisers."
-
+        elif event_type.name == self.MUSIC_ACT:
+            label = "music act"
+            help_text = f"{label}"
             # no video recording for music acts
             del self.fields["allow_video_recording"]
 
-        elif event_type.name == RECREATIONAL_EVENT:
-            # fix label and help_text for the title field
-            self.fields["title"].label = "Event Title"
-            self.fields["title"].help_text = "The title of this recreational event"
-
-            # fix label and help_text for the abstract field
-            self.fields["abstract"].label = "Event Abstract"
-            self.fields[
-                "abstract"
-            ].help_text = "The description/abstract of this recreational event."
-
-            # fix label and help_text for the submission_notes field
-            self.fields[
-                "submission_notes"
-            ].help_text = "Private notes regarding this recreational event. Only visible to yourself and the BornHack organisers."
-
+        elif event_type.name == self.RECREATIONAL_EVENT:
+            label = "recreational event"
+            help_text = f"{label}"
             # no video recording for music acts
             del self.fields["allow_video_recording"]
 
-        elif event_type.name in [TALK, LIGHTNING_TALK]:
-            # fix label and help_text for the title field
-            self.fields["title"].label = "Title of Talk"
-            self.fields["title"].help_text = "The title of this talk/presentation."
+        elif event_type.name in [self.TALK, self.LIGHTNING_TALK]:
+            label = "talk"
+            help_text = f"{label}"
+            # no duration for talks
+            del self.fields["duration"]
 
-            # fix label and help_text for the abstract field
-            self.fields["abstract"].label = "Abstract of Talk"
-            self.fields[
-                "abstract"
-            ].help_text = "The description/abstract of this talk/presentation. Explain what the audience will experience."
-
-            # fix label and help_text for the submission_notes field
-            self.fields[
-                "submission_notes"
-            ].help_text = "Private notes regarding this talk. Only visible to yourself and the BornHack organisers."
-
-            if self.fields.get("slides_url") and event_type.name == LIGHTNING_TALK:
+            if self.fields.get("slides_url") and event_type.name == self.LIGHTNING_TALK:
                 self.fields[
                     "slides_url"
                 ].help_text += " You will only get assigned a slot if you have provided slides (a title slide is enough if you don't use slides for the talk). You can add an URL later if need be."
 
-            # no duration for talks
-            del self.fields["duration"]
-
-        elif event_type.name == WORKSHOP:
-            # fix label and help_text for the title field
-            self.fields["title"].label = "Workshop Title"
-            self.fields["title"].help_text = "The title of this workshop."
-
-            # fix label and help_text for the submission_notes field
-            self.fields[
-                "submission_notes"
-            ].help_text = "Private notes regarding this workshop. Only visible to yourself and the BornHack organisers."
-
-            # fix label and help_text for the abstract field
-            self.fields["abstract"].label = "Workshop Abstract"
-            self.fields[
-                "abstract"
-            ].help_text = "The description/abstract of this workshop. Explain what the participants will learn."
-
+        elif event_type.name == self.WORKSHOP:
+            label = "workshop"
+            help_text = f"{label}"
             # no video recording for workshops
             del self.fields["allow_video_recording"]
 
-        elif event_type.name == RECREATIONAL_EVENT:
-            # fix label and help_text for the title field
-            self.fields["title"].label = "Event Title"
-            self.fields["title"].help_text = "The title of this recreational event."
-
-            # fix label and help_text for the submission_notes field
-            self.fields[
-                "submission_notes"
-            ].help_text = "Private notes regarding this recreational event. Only visible to yourself and the BornHack organisers."
-
-            # fix label and help_text for the abstract field
-            self.fields["abstract"].label = "Event Abstract"
-            self.fields[
-                "abstract"
-            ].help_text = "The description/abstract of this event. Explain what the participants will experience."
-
-            # no video recording for recreational events
-            del self.fields["allow_video_recording"]
-
-        elif event_type.name == MEETUP:
-            # fix label and help_text for the title field
-            self.fields["title"].label = "Meetup Title"
-            self.fields["title"].help_text = "The title of this meetup."
-
-            # fix label and help_text for the submission_notes field
-            self.fields[
-                "submission_notes"
-            ].help_text = "Private notes regarding this meetup. Only visible to yourself and the BornHack organisers."
-
-            # fix label and help_text for the abstract field
-            self.fields["abstract"].label = "Meetup Abstract"
-            self.fields[
-                "abstract"
-            ].help_text = "The description/abstract of this meetup. Explain what the meetup is about and who should attend."
-
-            # no video recording for meetups
+        elif event_type.name == self.MEETUP:
+            label = "meetup"
+            help_text = f"{label}"
+            # no video recording for workshops
             del self.fields["allow_video_recording"]
 
         else:
             raise ImproperlyConfigured(
                 "Unsupported event type, don't know which form class to use",
             )
+
+        # fix label and help_text for the name field
+        self.title_label_text(label)
+        self.title_help_text(help_text)
+
+        # fix label and help_text for the email field
+        self.abstract_label_text(label)
+        self.abstract_help_text(help_text)
+
+        # fix label and help_text for the submission_notes field
+        self.submission_notes_help_text(help_text)
+
